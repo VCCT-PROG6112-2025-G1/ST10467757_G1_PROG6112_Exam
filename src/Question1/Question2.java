05bff813ec599e7efe74db7c627b07f4f97de346
ColinSuther <sutherlandcolin781@gmail.com>
2025-11-10 13:16:21 +0200

    Final Commit - Question 2

---
+++ src/Question1/Question2.java
@@ -0,0 +1,69 @@
+package Question1;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.IOException;
+
+
+public class Question2 
+{
+
+    public static void main(String[] args) 
+    {   
+        /*
+        JFrame frame = new JFrame("Product Sales Application");
+        JLabel label = new JLabel("Years Processed:");
+        JTextArea output = new JTextArea();
+        JButton btnLoad = new JButton("Load Product Data");
+        JButton btnSave = new JButton("Save Product Data");
+        //Layout
+        frame.setLayout(new java.awt.FlowLayout());
+        frame.add(btnLoad);
+        frame.add(btnSave);
+        
+        int[][] productSales = new int[2][3];
+        productSales[0][0] = 300;
+        productSales[0][1] = 150;
+        productSales[0][2] = 700;
+        productSales[1][0] = 250;
+        productSales[1][1] = 200;
+        productSales[1][2] = 600;
+        
+        //event handlers
+        btnLoad.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e) 
+            {
+                try (PrintWriter pw = new PrintWriter(new FileWriter("data.txt"))) 
+                {
+                pw.println("300, 150, 700");
+                pw.println("250, 200, 600");
+                System.out.println("Sequential data written successfully to data.txt");
+                } 
+                catch (IOException e) 
+                {
+                System.out.println("An error occurred while writing to the file: " +
+                e.getMessage());
+                }
+            }
+        });
+        
+            
+        btnSave.addActionListener(new ActionListener() 
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                
+            }
+        });
+        frame.add(output);
+        frame.add(label);
+        output.setSize(300, 200);
+        frame.setSize(500, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        */
+    }
+    
+}
